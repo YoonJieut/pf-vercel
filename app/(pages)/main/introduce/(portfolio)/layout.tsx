07ea385d4fbea27bbffc52288e85f7a4727879f0
YoonJunHyeon <qmdldos@gmail.com>
2024-06-26 19:54:39 +0900

    test : layout.tsx 병렬 작성법 수정
    dynamic 로딩을 제거하고 공식 문서에 맞춰서 작성

--- app/(pages)/main/introduce/(portfolio)/layout.tsx
+++ app/(pages)/main/introduce/(portfolio)/layout.tsx
@@ -1,16 +1,18 @@
 import dynamic from "next/dynamic";
 
-const MenuComp = dynamic(() => import("./@menuComp/page"), { ssr: false });
+// const MenuComp = dynamic(() => import("./@menuComp/page"), { ssr: false });
 
 export default function PortfolioLayout({
   children,
+  MenuComp,
 }: {
   children: React.ReactNode;
+  MenuComp: React.ReactNode;
 }) {
   return (
     <div className="w-full h-11/12 flex">
       <section className="portfolioLeftSection w-1/2 h-full">
-        <MenuComp />
+        {MenuComp}
       </section>
 
       <section className="portfolioRightSection w-1/2  flex flex-col gap-[300px] overflow-x-hidden overflow-y-scroll text-[0.875rem] leading-[120%]">
