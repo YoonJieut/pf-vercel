f8408afed884a7ae9d856437289c522f0ae31f16
YoonJunHyeon <qmdldos@gmail.com>
2024-06-26 20:35:46 +0900

    test : 상태 변경 함수 추가 및 버튼 이벤트 추가

--- app/(pages)/main/introduce/portfolio/layout.tsx
+++ app/(pages)/main/introduce/portfolio/layout.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import MenuComp from "@/app/components/MenuComp";
 import { useState } from "react";
 import PortfolioPage from "./page";
@@ -7,8 +5,15 @@ import PortfolioPage from "./page";
 export default function PortfolioLayout() {
   const [state, setState] = useState(1);
 
+  const handleClick = () => {
+    setState(state + 1);
+  };
+
   return (
     <div className="w-full h-11/12 flex">
+      <button type="button" onClick={handleClick}>
+        Click me
+      </button>
       <section className="portfolioLeftSection w-1/2 h-full">
         <MenuComp />
       </section>
