9851af7fe5f4937ef33612aefff9f8ee83ecb54f
YoonJunHyeon <qmdldos@gmail.com>
2024-06-26 23:44:01 +0900

    feat : 홈 페이지 컴포넌트 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { CustomInputAreaVer2 } from "./components/ui/Molecules/FetchInput/CustomInputAreaVer2";
+import EntryFont from "./components/ui/design/EntryFont";
+
 export default function Home() {
   return (
     <div className="flex flex-col w-screen h-screen overflow-hidden p-3 justify-end">
-      HOME
+      <div className="flex flex-col">
+        <EntryFont />
+        <CustomInputAreaVer2 />
+      </div>
     </div>
   );
 }
