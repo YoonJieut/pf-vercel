61300c0b37507cb309640f80749e61cac27e45b4
YoonJunHyeon <qmdldos@gmail.com>
2024-06-27 18:47:20 +0900

    Docs : IntroducePage 내용 추가

--- app/(pages)/main/introduce/page.tsx
+++ app/(pages)/main/introduce/page.tsx
@@ -1,7 +1,31 @@
-export default function IntroduceTestPage() {
+import Image from "next/image";
+import Link from "next/link";
+
+import Section from "@/app/components/ui/atom/Basic/Section";
+import IntroMenu from "@/app/components/ui/Molecules/Menu/IntroMenu";
+import IntrodcueCircle from "@/app/components/ui/design/IntroduceCircle";
+
+export default function IntroducePage() {
   return (
     <main className="h-full w-full flex px-[100px] border border-sub-1 overflow-hidden">
-      introduce page
+      <Section className="leftIntro flex items-center w-1/2 h-full">
+        <Link href="/mainTest/aboutme">
+          <Image
+            src={"/images/kakao-talk-202402182120487351.png"}
+            alt="웹 개발자 윤준현입니다."
+            width={200}
+            height={200}
+          />
+        </Link>
+      </Section>
+      <Section className="rightIntro relative w-1/2 h-full">
+        {/* todo : 버튼에 따라 이미지가 달라져야 한다. */}
+        {/* 컴포넌트화 해야한다. */}
+        <div className="content w-full h-full flex flex-col items-end gap-14 mt-[140px]">
+          <IntroMenu />
+          <IntrodcueCircle />
+        </div>
+      </Section>
     </main>
   );
 }
